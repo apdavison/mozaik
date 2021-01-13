dca4121cdd2d6f8354cf98ff3b805ed6f1b2c115
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-01-13 19:22:04 +0100

    astype error

--- mozaik/sheets/population_selector.py
+++ mozaik/sheets/population_selector.py
@@ -71,7 +71,12 @@ class RCRandomN(PopulationSelector):
     })
         
     def generate_idd_list_of_neurons(self):
-        z = self.sheet.pop.all_cells.astype(int)
+        # z = self.sheet.pop.all_cells.astype(int)
+        if isinstance(self.sheet.pop.all_cells, list):
+            # z = numpy.asarray(self.sheet.pop.all_cells, dtype=numpy.int)
+            z = numpy.asarray(self.sheet.pop.all_cells)
+        else:
+            z = self.sheet.pop.all_cells.astype(int)
         mozaik.rng.shuffle(z)
         return z[:self.parameters.num_of_cells]
 
