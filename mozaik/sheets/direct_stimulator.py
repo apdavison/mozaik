93c425a8859921e68db0c88defedc867882d0860
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-02-09 15:49:33 +0100

    set parameters error 9

--- mozaik/sheets/direct_stimulator.py
+++ mozaik/sheets/direct_stimulator.py
@@ -266,8 +266,8 @@ class Kick(DirectStimulator):
                 print(self.ssae[i])
                 print(offset)
                 # self.ssae[i].set_parameters(spike_times=Sequence(a.astype(float)))
-                self.ssae[i].set(spike_times=Sequence(a.astype(float)))
-                # self.ssae[i].set(spike_times=[numpy.array([k]) for k in a.astype(float)])
+                # self.ssae[i].set(spike_times=Sequence(a.astype(float)))
+                self.ssae[i].set(spike_times=[numpy.array([k]) for k in a.astype(float)])
                 # self.ssae[i].set_parameters(spike_times=Sequence(a.astype(float)))
 
     def inactivate(self,offset):        
