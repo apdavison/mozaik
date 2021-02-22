003253354f12823b29bac544a27a6b34f2171fc3
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-02-22 17:05:52 +0100

    vogels python 3 update

--- examples/VogelsAbbott2005/run.py
+++ examples/VogelsAbbott2005/run.py
@@ -11,30 +11,32 @@ mpirun python run.py simulator_name number_processors parameters name_of_test
 For example:
 mpirun python run.py nest 2 param/defaults 'test'
 """
-import matplotlib
-matplotlib.use('Agg')
-
-# from mpi4py import MPI
-# from pyNN import nest
-from pyNN import spiNNaker
+import logging
 import sys
-import mozaik
+
 from mozaik.cli import parse_workflow_args
 from mozaik.controller import run_workflow, setup_logging
+from mozaik.storage.datastore import Hdf5DataStore, PickledDataStore
 from mozaik.tools.misc import result_directory_name
+# from mpi4py import MPI
+from parameters import ParameterSet
+# from pyNN import nest
+from pyNN import spiNNaker
+import matplotlib
+
+from analysis_and_visualization import perform_analysis_and_visualization
 from experiments import create_experiments
 from model import VogelsAbbott
-from mozaik.storage.datastore import Hdf5DataStore,PickledDataStore
-from analysis_and_visualization import perform_analysis_and_visualization
-from parameters import ParameterSet
 
-#mpi_comm = MPI.COMM_WORLD
-logger = mozaik.getMozaikLogger()
+
+matplotlib.use("Agg")
+# mpi_comm = MPI.COMM_WORLD
+logger = logging.getLogger(__name__)
 simulation_name = "VogelsAbbott2005"
 simulation_run_name, _, _, _, modified_parameters = parse_workflow_args()
 
 if True:
-    data_store,model = run_workflow(simulation_name,VogelsAbbott,create_experiments)
+    data_store, model = run_workflow(simulation_name, VogelsAbbott, create_experiments)
 else: 
     setup_logging()
     data_store = PickledDataStore(
