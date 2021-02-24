7092881b8ddec5e8c70835f54490f69585b499cf
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-02-24 19:27:21 +0100

    IDMixin error 4

--- mozaik/sheets/__init__.py
+++ mozaik/sheets/__init__.py
@@ -131,15 +131,15 @@ class Sheet(BaseComponent):
                 ]
 
             print(l[0])
-            print(type(l[0]))
+            print(type(l[0]))  # IDMixin object
             print(self.to_record.get(self.parameters.recorders[k].variables[0], []))
             print(type(self.to_record.get(self.parameters.recorders[k].variables[0], [])))
             print(self.parameters.recorders[k].variables[0])
             print(type(self.parameters.recorders[k].variables[0]))
 
             for var in self.parameters.recorders[k].variables:
-                self.to_record[var] = list(set(self.to_record.get(var, [])) | set(l))  # unhashable type: 'IDMixin'
-                # self.to_record[var] = list(frozenset(self.to_record.get(var, [])) | frozenset(l))
+                # self.to_record[var] = list(set(self.to_record.get(var, [])) | set(l))  # unhashable type: 'IDMixin'
+                self.to_record[var] = list(set(self.to_record.get(var, [])) | frozenset(l))
 
         # for k in self.to_record.keys():
         for k in self.to_record:
