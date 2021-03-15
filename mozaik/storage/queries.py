7f11efe9f9d18053f4d998232d6f7968c60c68d5
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-03-15 19:29:10 +0100

    mozaik_parametrized getattr error 4

--- mozaik/storage/queries.py
+++ mozaik/storage/queries.py
@@ -78,7 +78,8 @@ def param_filter_query(dsv, ads_unique=False, rec_unique=False, **kwargs):
         [(k[3:], kwargs[k]) for k in list(kwargs.keys()) if k[0:3] == "st_"]
     )
     kwargs = dict([(k, kwargs[k]) for k in list(kwargs.keys()) if k[0:3] != "st_"])
-
+    print(dsv)
+    print(dsv.block.segments)
     seg_st = [
         MozaikParametrized.idd(seg.annotations["stimulus"])
         for seg in dsv.block.segments
