5d2e85c347e97b312f9ca6bbe57a7310598d9982
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-03-23 16:13:04 +0100

    Parameterized error 15

--- mozaik/experiments/__init__.py
+++ mozaik/experiments/__init__.py
@@ -189,6 +189,10 @@ class NoStimulation(Experiment):
                                        })
 
     def __init__(self,model,parameters):
+        print("No stimulation 0")
+        print(parameters)
+        print(self.stimuli)
+        print("No stimulation 0 end")
         Experiment.__init__(self, model,parameters)
         self.stimuli.append(InternalStimulus(frame_duration=self.parameters.duration,
                                              duration=self.parameters.duration,
