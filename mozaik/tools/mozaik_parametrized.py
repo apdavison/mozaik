e93cc4c3bc30929d855b7dfdb409d34e507611fb
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-03-25 15:06:47 +0100

    visualization module python 3 update

--- mozaik/tools/mozaik_parametrized.py
+++ mozaik/tools/mozaik_parametrized.py
@@ -145,7 +145,7 @@ class MozaikParametrized(Parameterized):
         print(self.params())
         self.cached_get_param_values = None
         Parameterized.__init__(self, **params)
-        print(Parameterized.__init__(self, **params))
+        # print(Parameterized.__init__(self, **params))
         self.module_path = inspect.getmodule(self).__name__
         # print(self.name)
         # print(self.__class__.__name__)
