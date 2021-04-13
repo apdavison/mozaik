cc47804489d8e97ca52679c113c00135ee366671
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-04-13 15:08:15 +0200

    numpy broadcasting error 6

--- mozaik/connectors/modular.py
+++ mozaik/connectors/modular.py
@@ -74,7 +74,8 @@ class ModularConnector(Connector):
         v = ExpVisitor()
         v.visit(ast.parse(self.parameters.delay_expression))
         self.delay_function_names = v.names
-
+        print("self.source ", self.source)
+        print("self.target ", self.target)
         for k in self.weight_function_names:
             self.weight_functions[k] = load_component(
                 self.parameters.weight_functions[k].component
