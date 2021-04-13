ee5c1c6ba807ae06b409caf87f0b5049385cdbdf
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-04-13 15:21:05 +0200

    mozaik core python 3 update

--- mozaik/__init__.py
+++ mozaik/__init__.py
@@ -124,8 +124,8 @@ def load_component(path):
     path_parts = path.split(".")
     module_name = ".".join(path_parts[:-1])
     class_name = path_parts[-1]
-    # _module = import_module(module_name)
-    _module = __import__(module_name, globals(), locals(), [class_name], -1)
+    _module = import_module(module_name)
+    # _module = __import__(module_name, globals(), locals(), [class_name], -1)
     logger.info("Loaded component %s from module %s" % (class_name, module_name))
     return getattr(_module, class_name)
 
