1b890fcfeafea24b0ff9b0c6f9e5ae681ea21e87
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-04-21 16:55:26 +0200

    maximum delay error 7

--- mozaik/models/vision/spatiotemporalfilter.py
+++ mozaik/models/vision/spatiotemporalfilter.py
@@ -479,14 +479,14 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
             # print("dir(self.sheets[rf_type]) ", dir(self.sheets[rf_type]))
             # print("seeds ", seeds)
             # print("self.sheets[rf_type].pop.all_cells ", self.sheets[rf_type].pop.all_cells)
-            # if not self.parameters.mpi_reproducible_noise:
+            if not self.parameters.mpi_reproducible_noise:
                 #    ncs = sim.NoisyCurrentSource(**self.parameters.noise)
                 #    ncs = sim.SpikeSourcePoisson  # test this
                 #    sim.SpikeSourcePoisson
                 #    np = sim.Population.SpikeSourcePoisson(rate=0)
 
-            #    np = sim.Population(10, sim.SpikeSourcePoisson(rate=10.0), label="noise")
-            #    sim.Projection(np, self.sheets[rf_type].pop, sim.AllToAllConnector())
+                np = sim.Population(10, sim.SpikeSourcePoisson(rate=10.0), label="noise")
+                sim.Projection(np, self.sheets[rf_type].pop, sim.AllToAllConnector())
 
             # print("SpikeSourcePoisson")
             # self.sheets[rf_type].pop(sim.SpikeSourcePoisson(rate=0))
@@ -754,7 +754,7 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                 )
 
             for i, (scs, ncs) in enumerate(zip(self.scs[rf_type], self.ncs[rf_type])):
-                scs.set_parameters(times=times, amplitudes=zers + amplitude, copy=False)
+                scs.set_parameters(times=times, amplitudes=zers + amplitude, copy=False)  # this has to chenge
                 if self.parameters.mpi_reproducible_noise:
                     t = numpy.arange(0, duration, ts) + offset
 
