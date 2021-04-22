78ffa6315f313245e63a5993cba813f081e90bf7
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-04-22 18:44:53 +0200

    maximum delay error 15

--- mozaik/connectors/modular.py
+++ mozaik/connectors/modular.py
@@ -128,7 +128,7 @@ class ModularConnector(Connector):
         delays = (
             numpy.rint(delays / self.simulator_time_step) * self.simulator_time_step
         )
-        print("delays in connectors modular ", delays)
+        # print("delays in connectors modular ", delays)
         return delays
 
     def _connect(self):
