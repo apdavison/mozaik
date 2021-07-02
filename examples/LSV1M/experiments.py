3556a540f0bfa33173f8f25b9e404aca994100a2
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-07-02 13:48:49 +0200

    reduce number of trials in experiments 6

--- examples/LSV1M/experiments.py
+++ examples/LSV1M/experiments.py
@@ -17,7 +17,7 @@ def create_experiments(model):
 
         # Measure orientation tuning with full-filed sinusoidal gratins
         MeasureOrientationTuningFullfield(model, ParameterSet(
-             {'num_orientations': 5, 'spatial_frequency': 0.8, 'temporal_frequency': 2, 'grating_duration': 2*143*7, 'contrasts': [30, 100], 'num_trials':5})),
+             {'num_orientations': 5, 'spatial_frequency': 0.8, 'temporal_frequency': 2, 'grating_duration': 2*143*7, 'contrasts': [30, 100], 'num_trials':10})),
         # 'num_orientations': 10
         # Measure response to natural image with simulated eye movement
         MeasureNaturalImagesWithEyeMovement(model, ParameterSet(
