27b121d255f95f7d562e2bdc03090cddff135239
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-07-26 16:55:00 +0200

    remove initial step current injection

--- mozaik/models/vision/spatiotemporalfilter.py
+++ mozaik/models/vision/spatiotemporalfilter.py
@@ -471,7 +471,8 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
             for i, lgn_cell in enumerate(self.sheets[rf_type].pop.all_cells):
                 if i == 0:
                    break
-                scs = sim.StepCurrentSource(times=[0.0], amplitudes=[0.0])
+                # scs = sim.StepCurrentSource(times=[0.0], amplitudes=[0.0])
+                scs = {}
 
                 # if not self.parameters.mpi_reproducible_noise:
                     # print("noisy current created")
@@ -484,7 +485,7 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                     self.scs[rf_type].append(scs)
                     # self.ncs[rf_type].append(ncs)
                 # lgn_cell.inject(scs)
-                scs.inject_into(lgn_cell)
+                # scs.inject_into(lgn_cell)
                 # lgn_cell.inject(ncs)
 
             # inject noisy current to populations
