d0b05f937dd8abc5738ce728d68a7c566a4b94d5
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-08-02 16:01:28 +0200

    use only noisy current injection

--- mozaik/models/vision/spatiotemporalfilter.py
+++ mozaik/models/vision/spatiotemporalfilter.py
@@ -471,8 +471,8 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
             for i, lgn_cell in enumerate(self.sheets[rf_type].pop.all_cells):
                 # if i == 0:
                 #   break
-                # scs = sim.StepCurrentSource(times=[0.0], amplitudes=[0.0])
-                scs = {}
+                scs = sim.StepCurrentSource(times=[0.0], amplitudes=[0.0])
+                # scs = {}
 
                 # if not self.parameters.mpi_reproducible_noise:
                     # print("noisy current created")
@@ -484,7 +484,7 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                     # self.ncs_rng[rf_type].append(numpy.random.RandomState(seed=seeds[i]))
                     self.scs[rf_type].append(scs)
                     # self.ncs[rf_type].append(ncs)
-                # lgn_cell.inject(scs)
+                lgn_cell.inject(scs)
                 # scs.inject_into(lgn_cell)
                 # lgn_cell.inject(ncs)
 
@@ -969,7 +969,7 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
             input_cells[rf_type].initialize(visual_space.background_luminance, duration)
 
         for rf_type in self.rf_types:
-            # break
+            break
             if self.parameters.gain_control.non_linear_gain != None:
                 amplitude = self.parameters.linear_scaler * self.parameters.gain_control.non_linear_gain.luminance_gain * numpy.sum(
                     input_cells[rf_type].receptive_field.kernel.flatten()) * visual_space.background_luminance / (
