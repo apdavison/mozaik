dc2eeee6c3c0823f55d09053b5ad546c1f7788fb
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-08-04 16:16:05 +0200

    test step current injection 3

--- mozaik/models/vision/spatiotemporalfilter.py
+++ mozaik/models/vision/spatiotemporalfilter.py
@@ -741,13 +741,13 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                     input_cells[rf_type].receptive_field.kernel.flatten()) * visual_space.background_luminance
 
             # for i, (scs, ncs) in enumerate(zip(self.scs[rf_type], self.ncs[rf_type])):
-            for i, scs in enumerate(self.scs[rf_type]):
-                # for lgn_cell in self.sheets[rf_type].pop.all_cells:
-                break
+            # for i, scs in enumerate(self.scs[rf_type]):
+            for lgn_cell in self.sheets[rf_type].pop.all_cells:
+                # break
                 # scs.set_parameters(times=times, amplitudes=zers + amplitude, copy=False)
-                scs.set_parameters(times=times, amplitudes=zers + amplitude)
-                # scs = self.model.sim.StepCurrentSource(times=times, amplitudes=zers + amplitude)
-                # lgn_cell.inject(scs)
+                # scs.set_parameters(times=times, amplitudes=zers + amplitude)
+                scs = self.model.sim.StepCurrentSource(times=times, amplitudes=zers + amplitude)
+                lgn_cell.inject(scs)
                 # if self.parameters.mpi_reproducible_noise:
                 #    print("noisy current modified in null input")
                 #    t = numpy.arange(0, duration, ts) + offset
