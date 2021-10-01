e2de0ba2139ba9c8285958e9c66f991aa68c87e6
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-10-01 13:09:39 +0200

    check multiple step currents

--- mozaik/models/vision/spatiotemporalfilter.py
+++ mozaik/models/vision/spatiotemporalfilter.py
@@ -668,8 +668,8 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                     print("times for first", t)
                 # scs.set_parameters(times=t, amplitudes=a)
                 # lgn_cell.inject(scs)
-                t = [10000]
-                a = [10]
+                # t = [10000]
+                # a = [10]
                 scs = self.model.sim.StepCurrentSource(times=t, amplitudes=a)
                 # self.scs[rf_type].append(scs)
                 lgn_cell.inject(scs)
