812878904d2a1f05f9f549ba5ec069711c787368
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2021-10-29 11:07:52 +0200

    record v in all populations

--- mozaik/sheets/__init__.py
+++ mozaik/sheets/__init__.py
@@ -175,6 +175,7 @@ class Sheet(BaseComponent):
                     self.to_record[var] = [i.id for i in self.pop.all_cells]
                     # print("list ot record for all spikes ", self.to_record[var])
                 else:
+                    print("record other than spikes or v", var)
                     # print("list(set(self.to_record.get(var, [])) | set(l)) ", list(set(self.to_record.get(var, [])) | set(l)))
                     self.to_record[var] = list(set(self.to_record.get(var, [])) | set(l))  # unhashabse type: 'IDMixin'
         # print(self.to_record)
